a2a78d852aed67b8e249d648ac995222d5fb87ff
SandraHdz2 <38160566+SandraHdz2@users.noreply.github.com>
2018-04-14 13:16:30 -0500

    Add files via upload

---
+++ concatenar_str.py
@@ -0,0 +1,12 @@
+#concatenar para str es +
+
+a=input ("Que quieres imprimir?")
+b=int (input("¿Cuantas veces quieres imprimirlo?"))
+imp=a*b
+print (imp)
+
+
+a=input ("Que quieres imprimir?")
+b=int (input("¿Que caracter quiere visualizar?"))
+#imp=a*b
+print ("Caracter ",a[b])
