46963586c01ae88bb59e1f331ffaa73f634a981c
SandraHdz2 <38160566+SandraHdz2@users.noreply.github.com>
2018-04-14 11:39:10 -0500

    Add files via upload

---
+++ castear.py
@@ -0,0 +1,9 @@
+float1=1.5
+entero=25
+print ("Tu numero entero es",entero,"\nTu numero decimal es:",float1)
+entero2=int (float1)
+float2=float (entero)
+print ("Tu tipo entero es",entero,type(float2))
+print ("Tu tipo decimal es",float1,type(entero2))
+
+#casteo:tipo de dato a otro
---
+++ tipodato.py
@@ -0,0 +1,6 @@
+largo=1
+corto=100000000000000000000000000*10^19
+print("Largo",largo,"Su tipo es",type(largo))
+print("Corto",corto,"Su tipo es",type (corto))
+input("...")
+
