d98e465455c1912e21c1c8eb38f2b072ba8ee14c
SandraHdz2 <38160566+SandraHdz2@users.noreply.github.com>
2018-04-14 13:53:50 -0500

    Add files via upload

---
+++ listas_arrays.py
@@ -0,0 +1,21 @@
+name="Sandra"#0
+apell=" Hernandez"#1
+estatura=1.65#2
+no_c=314050072#3
+frase="Se feliz"#4
+lista=[name,apell,estatura,no_c,frase]
+#len es para ver el tamano de la lista
+print (lista,type(lista))
+#appened agrega el objeto de la lista al final de esta
+#lista.appened (90)
+#index devuelve el indice del objeto enviado
+print (lista.index(apell))
+#count cuantos objetos hay en la lista
+
+#agrega uno o varios al final de la lista .extend
+#print(lista.extend (1,2,3))
+
+#insert(indice,objeto)
+print(lista.insert(3,"aiuda"))
+#pop(indice) devuelve el valor de la lista y lo elimina, es el ultimo
+#remove(objeto) elimina el primer objeto eliminado
