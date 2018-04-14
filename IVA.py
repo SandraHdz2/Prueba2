8452275bfd31d0ceabe8691c28d9358afc11c680
SandraHdz2 <38160566+SandraHdz2@users.noreply.github.com>
2018-04-14 11:06:05 -0500

    Add files via upload

---
+++ IVA.py
@@ -0,0 +1,15 @@
+"""iva=float (input("1.Ingrese el IVA:"))
+cantidad= float (input("2.Ingrese la cantidad:"))
+print ("La cantidad es:",cantidad)
+print ("El iva es:",iva*cantidad)
+input("Presiona cualquier tecla para continuar....")"""
+"""boolean=True
+print (boolean)
+boolean=False
+print(boolean)
+"""
+no1=15
+cadena="hola"
+float=1.36
+estado=True
+
